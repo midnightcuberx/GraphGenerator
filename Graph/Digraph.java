34dc09a2889a0bc5edbe7f71e469f21ebc22e404
Veni <drcubing27@gmail.com>
2023-11-14 21:05:15 +1300

    Digraph drawLine
    Digraph now draws arrows

--- Graph/Digraph.java
+++ Graph/Digraph.java
@@ -64,7 +64,35 @@ public class Digraph extends Graph{
     }
 
     public void drawLines(Graphics g, int i, int j){
-        g.drawLine(getX(i) + nodeHeight / 2, getY(i) + nodeHeight / 2, getX(j) + nodeHeight / 2, getY(j) + nodeHeight / 2);
+        int arrowLength = 10;
+
+        int x1, x2, y1, y2;
+        x1 = getX(i) + nodeHeight / 2;
+        y1 = getY(i) + nodeHeight / 2;
+        x2 = getX(j) + nodeHeight / 2;
+        y2 = getY(j) + nodeHeight / 2;
+
+        int diffX = x1 - x2;
+        int diffY = y1 - y2;
+        double hypotenuse = Math.sqrt(diffX * diffX + diffY * diffY);
+        double lineLength = nodeHeight / 2 + 10;
+        double xShift = lineLength * diffX / hypotenuse;
+        double yShift = lineLength * diffY / hypotenuse;
+        g.drawLine(x1 - (int) xShift, y1 - (int) yShift, x2 + (int) xShift, y2 + (int) yShift);
+
+        double angle = Math.atan2(diffY, diffX); //+-20
+        int x = x2 + (int) xShift;
+        int y = y2 + (int) yShift;
+        double deg1 = angle + Math.toRadians(20);
+        double deg2 = angle - Math.toRadians(20);
+
+        double arrowX1 = Math.cos(deg1) * arrowLength;
+        double arrowX2 = Math.cos(deg2) * arrowLength;
+        double arrowY1 = Math.sin(deg1) * arrowLength;
+        double arrowY2 = Math.sin(deg2) * arrowLength;
+
+        g.drawLine(x + (int) arrowX1, y + (int) arrowY1, x, y);
+        g.drawLine(x + (int) arrowX2, y + (int) arrowY2, x, y);
     }
 
 }
